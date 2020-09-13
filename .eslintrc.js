bb04ff5a626c832d4bce467746b68122fb7ce3f2
APOORVACHANDRU <47515059+APOORVACHANDRU@users.noreply.github.com>
2020-09-13 13:13:22 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": ["airbnb", "prettier", "plugin:prettier/recommended"],
+    "plugins": [
+        "prettier"
+      ],
+      "rules": {
+        "prettier/prettier": "error"
+      },
+};
